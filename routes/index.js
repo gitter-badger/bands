1e2ae3dedc654c476444861c9c65087c0bf33958
Clay Stewart <cstewart@rosettastone.com>
2014-12-08 23:07:08 -0600

    Add events route to query to bring back bandsintown api results for sob

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var request = require("request");
 
 // require route files for resources
 module.exports = function(router){
@@ -11,8 +12,20 @@ module.exports = function(router){
     res.render('index');
   });
 
-  router.get('/events', function() {
+  router.get('/events', function(req, res) {
+    var url = 'http://api.bandsintown.com/artists/Sons%20of%20Bill/events.json?api_version=2.0&app_id=sonsofbill_email_html_generator',
+        requestOptions = {
+          url: url,
+          json: true
+        };
 
+    request(requestOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        res.send(body);
+      } else {
+        res.send({ success: false, message: 'Unknown Error getting result from bandsintown api.'});
+      }
+    })
   });
 
 };
